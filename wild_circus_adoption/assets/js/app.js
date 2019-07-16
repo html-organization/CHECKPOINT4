d8d0ec3a21e7af371af3870182f0a0155f6d30bf
EmilieVilliere <you@example.com>
2019-07-16 18:06:02 +0200

    adding navbar and carousel

--- wild_circus_adoption/assets/js/app.js
+++ wild_circus_adoption/assets/js/app.js
@@ -6,9 +6,21 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+require('bootstrap');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+const $ = require('jquery');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+//Nav opacity on scroll
+
+$(window).scroll(function () {
+    let windScroll = $(window).scrollTop();
+    if (windScroll >= 50) {
+        $('header').addClass('fixed');
+    } else {
+        $('header').removeClass('fixed');
+    }
+});
+
